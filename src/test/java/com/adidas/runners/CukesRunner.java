97e0b2195907a1e9fd2c72f0af3986b123bee3c1
merkurt2001 <merkurt2001@gmail.com>
2021-12-13 22:28:42 +0300

    tag added

--- src/test/java/com/adidas/runners/CukesRunner.java
+++ src/test/java/com/adidas/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/adidas/stepdefinitions",
         dryRun = false,
-        tags = ""
+        tags = "@TRN-1243"
 
 
 )
